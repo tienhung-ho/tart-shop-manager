591049b00a215ec4f43a297b972f06cfec680f59
tienhung-ho <hohung1203@gmail.com>
2024-08-28 13:03:48 +0700

    Add caching utility functions - Add generate_key.go in internal/util/cache

--- internal/util/cache/generate_key.go
+++ internal/util/cache/generate_key.go
@@ -1,1 +1,23 @@
-package util
+package cacheutil
+
+import (
+	"fmt"
+	commonfilter "tart-shop-manager/internal/common/filter"
+	paggingcommon "tart-shop-manager/internal/common/paging"
+)
+
+func GenerateKey(entityName string, cond map[string]interface{}, paging paggingcommon.Paging,
+	filter commonfilter.Filter, morekeys ...string) string {
+
+	key := fmt.Sprintf("%s:", entityName)
+	for k, v := range cond {
+		key += fmt.Sprintf("%s=%v:", k, v)
+	}
+
+	key += fmt.Sprintf("%s=%v:", "page", paging.Page)
+	key += fmt.Sprintf("%s=%v:", "limit", paging.Limit)
+
+	key += fmt.Sprintf("%s=%v:", "filter", filter.Status)
+
+	return key
+}
