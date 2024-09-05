f5406b57933767201ff7d864c9f0d13a8bc48e50
tienhung-ho <hohung1203@gmail.com>
2024-09-05 19:25:27 +0700

    Refactor: Update Role schema and create role logic - Update Redis and SQL schema for role entities

--- internal/entity/dtos/redis/role/create_role.go
+++ internal/entity/dtos/redis/role/create_role.go
@@ -1,4 +1,14 @@
-package rolerdbmodel
+package rolecachemodel
+
+import (
+	"tart-shop-manager/internal/common"
+	permissionmodel "tart-shop-manager/internal/entity/dtos/sql/permission"
+)
 
 type CreateRole struct {
+	RoleID      uint                         `json:"role_id"`
+	Name        string                       `json:"name"`
+	Description string                       `json:"description"`
+	Permissions []permissionmodel.Permission `gorm:"many2many:role_permission;"`
+	*common.CommonFields
 }
--- internal/entity/dtos/sql/role/role.go
+++ internal/entity/dtos/sql/role/role.go
@@ -1,1 +1,39 @@
-package role
+package rolemodel
+
+import (
+	"tart-shop-manager/internal/common"
+	rolecachemodel "tart-shop-manager/internal/entity/dtos/redis/role"
+	permissionmodel "tart-shop-manager/internal/entity/dtos/sql/permission"
+)
+
+const (
+	EntityName = "role"
+)
+
+type Role struct {
+	RoleID      uint                         `gorm:"column:role_id;primaryKey;autoIncrement" json:"role_id"`
+	Name        string                       `gorm:"column:name;size:255;not null;unique" json:"name"`
+	Description string                       `gorm:"column:description" json:"description"`
+	Permissions []permissionmodel.Permission `gorm:"many2many:role_permission;"`
+	*common.CommonFields
+}
+
+func (Role) TableName() string {
+	return "Role"
+}
+
+func (r Role) ToCreateRoleCache() *rolecachemodel.CreateRole {
+	return &rolecachemodel.CreateRole{
+		RoleID:      r.RoleID,
+		Name:        r.Name,
+		Description: r.Description,
+		Permissions: r.Permissions,
+		CommonFields: &common.CommonFields{
+			CreatedAt: r.CreatedAt,
+			UpdatedAt: r.UpdatedAt,
+			Status:    r.Status,
+			CreatedBy: r.CreatedBy,
+			UpdatedBy: r.UpdatedBy,
+		},
+	}
+}
