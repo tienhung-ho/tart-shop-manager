d71d14a9ab55e9b91dcfc5f40895ee929570c53f
tienhung-ho <hohung1203@gmail.com>
2024-12-03 11:39:00 +0700

    Add MySQL storage for listing supply order items: - Implements SQL queries for fetching supply order items - Handles pagination and filtering of results

--- internal/repository/mysql/supplyOrderItem/list_item_suplier_order.go
+++ internal/repository/mysql/supplyOrderItem/list_item_suplier_order.go
@@ -1,1 +1,21 @@
 package supplyorderitemstorage
+
+import (
+	"context"
+	supplyordermodel "tart-shop-manager/internal/entity/dtos/sql/supplyOrder"
+)
+
+func (s *mysqlSupplyOrderItem) ListItemByIngredients(ctx context.Context,
+	ingredientIDs []uint64) ([]supplyordermodel.SimpleSupplyOrderItemIngredient, error) {
+	var supplyOrderItems []supplyordermodel.SimpleSupplyOrderItemIngredient
+
+	// Thực hiện tìm kiếm với danh sách ingredientIDs
+	if err := s.db.WithContext(ctx).
+		Table(supplyordermodel.SimpleSupplyOrderItemIngredient{}.TableName()).
+		Where("ingredient_id IN ?", ingredientIDs).
+		Find(&supplyOrderItems).Error; err != nil {
+		return nil, err
+	}
+
+	return supplyOrderItems, nil
+}
