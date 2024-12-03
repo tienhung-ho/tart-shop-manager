8cfca79575af7b82fe3257ebe8646682dd7166e3
tienhung-ho <hohung1203@gmail.com>
2024-12-03 11:39:24 +0700

    Update permission router with new routes: - Adds endpoints for permission-related operations - Integrates with newly added handlers and business logic

--- api/router/v1/permission/permission.go
+++ api/router/v1/permission/permission.go
@@ -1,1 +1,12 @@
-package permission
+package permissionv1
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
+	permissionhandler "tart-shop-manager/api/handler/permission"
+)
+
+func PermissionRouter(permission *gin.RouterGroup, db *gorm.DB, rdb *redis.Client) {
+	permission.GET("/list", permissionhandler.ListItemPermission(db, rdb))
+}
